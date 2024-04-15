dbc585b5587cb58191b0173f6fda36f2d8348ae0
임재현 <163499219+hamtory05@users.noreply.github.com>
2024-04-15 23:20:35 +0900

    Update 5855.py

--- jaehyun/2024-04-09/5855.py
+++ jaehyun/2024-04-09/5855.py
@@ -1,1 +1,12 @@
+stuff = int(input()) # 물건 가격
+money = 1000 
+changes = [500, 100, 50, 10, 5, 1]
+change_money = money - stuff # 거스름돈
+save = 0 # 잔돈 갯수
 
+for i in changes:
+    if change_money >= i:
+        save += change_money // i
+        change_money %= i 
+
+print(save)
