cfc00772ef8a9892728bbbd0ad5d35e350899168
임재현 <163499219+hamtory05@users.noreply.github.com>
2024-04-15 23:20:59 +0900

    Update 16435.py

--- jaehyun/2024-04-09/16435.py
+++ jaehyun/2024-04-09/16435.py
@@ -1,1 +1,13 @@
+# 과일의 개수, 스네이크버드 초기 길이
+fruit_num, snake_length = map(int, input().split())
+# 과일 높이
+height = list(map(int, input().split()))
+height.sort()
 
+for i in range(len(height)):
+    if snake_length >= height[i]:
+        snake_length += 1
+    else:
+        break
+    
+print(snake_length)
