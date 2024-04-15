3c58505d8cca4b1f50a6f9c65edcb9c49554e509
임재현 <163499219+hamtory05@users.noreply.github.com>
2024-04-16 01:53:36 +0900

    Update 16953.py

--- jaehyun/2024-04-09/16953.py
+++ jaehyun/2024-04-09/16953.py
@@ -1,1 +1,15 @@
+A, B = map(int, input().split())
+save = 0
 
+while A != B:
+    if B % 2 != 0:
+        B = (B - 1) / 10
+        save += 1
+        if B < A:
+            print("-1")
+            exit()
+    else:
+        B = B / 2
+        save += 1
+
+print(save + 1)
