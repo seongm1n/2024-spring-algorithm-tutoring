84e8eb72257e3925987057729ca8f6faafac135b
임지수 <128721747+JISUWITHALLCAPS@users.noreply.github.com>
2024-04-18 20:19:55 +0900

    Update boj-16953.py

--- Jisu/boj-16953.py
+++ Jisu/boj-16953.py
@@ -2,7 +2,7 @@ a,b = map(int,input().split())
 cnt = 1
 while a != b:
     cnt += 1
-    if b == 0:
+    if b < a:
         cnt = -1
         break
     if b % 10 == 1:
@@ -12,5 +12,4 @@ while a != b:
     else:
         cnt = -1
         break
-
 print(cnt)
